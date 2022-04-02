504ee6fde5f54990d1602f3c2742a3b83731c049
Zuzannamila <zuza.mila@onet.pl>
2022-04-02 20:08:24 +0100

    Added Login endpoint

--- quiz-manager/Controllers/IdentityController.cs
+++ quiz-manager/Controllers/IdentityController.cs
@@ -7,10 +7,44 @@ namespace quiz_manager.Controllers
     public class IdentityController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
-        public IdentityController(UserManager<IdentityUser> userManager)
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public IdentityController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
+
+        /// <summary>
+        /// Endpoint responsible for registering new user
+        /// </summary>
+        /// <param name="item">User data</param>
+        /// <response code="201">Empty response signaling the user was succesfully saved.</response>
+        [ProducesErrorResponseType(typeof(void))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var userFromDb = await _userManager.FindByEmailAsync(model.Email);
+            if (userFromDb == null)
+            {
+                return BadRequest();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(userFromDb, model.Password, false);
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+            return Ok(new
+            {
+                result = result,
+                username = userFromDb.UserName,
+                useremail = userFromDb.Email,
+                token = "Token goes here"
+            }); 
+        }
+
 
         /// <summary>
         /// Endpoint responsible for registering new user
---
+++ quiz-manager/ViewModels/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace quiz_manager.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
